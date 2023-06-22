8c62e59a9bb0862c0eebe6e249d11f2342d09a5d
mohithworks <mohithworks2002@gmail.com>
2023-06-22 23:01:18 +0530

    Changes

--- src/utils/getAuthorSlugv2.ts
+++ src/utils/getAuthorSlugv2.ts
@@ -8,8 +8,13 @@ const getAuthorSlug = () => {
 
     const url = process.env.NEXT_URL;
 
+    console.log('location1', location)
+    console.log('location2', location2)
+    console.log('url', url)
+
     const authorSlug:any = location != url ? location2 !== process.env.NEXT_SITE_NAME ? { custom_domain: true, domain1: location, domain2: location2 } : { custom_domain: false, domain1: location, domain2: 'no' } : { custom_domain: false, domain1: 'hrithik', domain2: 'no' };
 
+    console.log('authorSlug', authorSlug)
     return authorSlug;
 }
 
